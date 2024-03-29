5d3bf5d3c64166f77cd5e8b94b03a29fc0e753eb
shubhra tiwary <shubhratiwary5@gmail.com>
2024-03-29 16:26:49 +0530

    random commit3

--- frontend/ndefined/src/components/Connecting.jsx
+++ frontend/ndefined/src/components/Connecting.jsx
@@ -5,6 +5,8 @@ export default function Connecting(){
          <div className="containing-container">
             <div className='connecting-text-container'>
                 <p className='Connecting-text'>Connecting <br></br>Globally</p>
+
+                
             </div>
             <div className='connecting-image-container'>
                 <img src='connecting-golabally.gif' className='connecting-img'/>
